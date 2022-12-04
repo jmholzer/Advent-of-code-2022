745640e37f8590f91e94973eca5b47a8c1e565f1
Jannic Holzer <jannic.holzer@quantumblack.com>
2022-12-04 01:02:02 +0000

    Add solution for day 1 part 1

---
+++ day-1/part_1.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def __init__(self, input_file_path: str) -> None:
+        self._input = _read_input(input_file_path)
+
+    def solve(self):
+        result = -float("inf")
+        calories = 0
+        for line in self._input:
+            if line == "":
+                result = max(result, calories)
+                calories = 0
+            else:
+                calories += int(line)
+        return result
+
+
+def _read_input(input_file_path: str) -> List[str]:
+    with open(input_file_path, "r") as input_file:
+        for line in input_file:
+            yield line.strip()
+
+
+if __name__ == "__main__":
+    solution = Solution("input")
+    print(solution.solve())
